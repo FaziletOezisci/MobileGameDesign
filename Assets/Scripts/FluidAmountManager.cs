6d5d32963059a478f05374f243efbf5d401f54be
Lidia Diaz Navarro <lyd044@gmail.com>
2018-06-08 15:54:05 +0200

    imported package

---
+++ Assets/Scripts/FluidAmountManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidAmountManager : MonoBehaviour {
+
+    public ParticleSystem water;
+    public int waterAmount = 10;
+
+	// Update is called once per frame
+	void Start () {
+        water.Emit(waterAmount);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //public float speed = 1.5f;
+
+    Vector3 acceleration;
+    float speed = 50.0f;
+
+	void Update () {
+        RotateScenario();
+    }
+
+    public void RotateScenario() {
+        acceleration = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+        Debug.Log(acceleration);
+        transform.rotation = Quaternion.Euler(0, 0, -1 * (Input.acceleration.x * speed) ); // To do: controlar el incremento de la rotacion.
+
+
+        //float acceleration = Input.acceleration.x;
+        //transform.Rotate(0, 0, -1 * acceleration / speed );
+
+    }
+}
