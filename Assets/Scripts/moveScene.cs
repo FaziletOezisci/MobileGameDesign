b9d0d84154f56d0a75c019ff451599265b92f238
faziletOezisci <hdm@HdM-MacBook.local>
2018-06-18 12:30:42 +0200

    added key control mechanics
    just for testing de mechanics. Next step is to replace the key control with the smartphone accelerometer.

---
+++ Assets/Scripts/moveScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.Rotate(0f,0f, -Input.GetAxis("Horizontal"));
+	}
+}
