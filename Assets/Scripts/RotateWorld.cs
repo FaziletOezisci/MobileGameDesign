f3f7c4cba128e9e8ac329450fa906c68572465df
faziletOezisci <hdm@HdM-MacBook.fritz.box>
2018-08-06 23:56:34 +0200

    rotate scene with device accelerometer

---
+++ Assets/Scripts/RotateWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWorld : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float temp = Input.acceleration.x;
+        //float z = Input.acceleration.z;
+        //Debug.Log(temp);
+        //transform.Translate(0, 0, -z * speed * Time.deltaTime);
+        transform.Rotate(0, 0, -temp * speed);
+
+	}
+
+}
