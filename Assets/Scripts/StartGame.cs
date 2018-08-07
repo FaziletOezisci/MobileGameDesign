6675aeee50bb65e0737fa6f0f1c8a68c08bdc525
Lidia Diaz Navarro <lyd044@gmail.com>
2018-08-07 17:25:15 +0200

    - working on StartScreen scene: added graphics - Created Script StartGame

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        //If user clicks on screen, the lv between "" will be loaded 
+        SceneManager.LoadScene("Level1");
+
+    }
+
+}
