00140d16d6bae2038b1ef863bf6ee515432adf2e
Lidia Diaz Navarro <lyd044@gmail.com>
2018-08-09 22:21:47 +0200

    -made transition from Title Screen to credits.

---
+++ Assets/Plugins/CameraMover/CameraMover.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour 
+{
+	public Transform[] Transforms;
+
+	private int currentIndex;
+
+	void Awake()
+	{
+		SnapToPosition(0);
+	}
+
+	public void SnapToPosition(int index)
+	{
+		this.currentIndex = index;
+		this.transform.position = this.Transforms[index].position;
+		this.transform.rotation = this.Transforms[index].rotation;
+	}
+
+	public IEnumerator MoveToPosition(int index, float time)
+	{
+		if (this.currentIndex == index)
+		{
+			yield break;
+		}
+		Vector3 startPosition = this.transform.position;
+		Quaternion startRotation = this.transform.rotation;
+		Vector3 endPosition = this.Transforms[index].position;
+		Quaternion endRotation = this.Transforms[index].rotation;
+
+		this.currentIndex = index;
+		float elapsedTime = 0;
+		while (elapsedTime < time)
+		{
+			float a = elapsedTime / time;
+			this.transform.position = Vector3.Lerp(startPosition, endPosition, a);
+			this.transform.rotation = Quaternion.Slerp(startRotation, endRotation, a);
+
+			yield return new WaitForEndOfFrame();
+			elapsedTime += Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Plugins/CameraMover/MyGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyGUI : MonoBehaviour 
+{
+	public CameraMover MyCameraMover;
+
+	void OnGUI()
+	{
+		if (GUILayout.Button("Move to 0"))
+		{
+			this.MyCameraMover.StartCoroutine(this.MyCameraMover.MoveToPosition(0, 1f));
+		}
+		if (GUILayout.Button("Move to 1"))
+		{
+			this.MyCameraMover.StartCoroutine(this.MyCameraMover.MoveToPosition(1, 1f));
+		}
+	}
+}
