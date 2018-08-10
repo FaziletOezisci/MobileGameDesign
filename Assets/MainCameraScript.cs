6026f15adeb0d2782054c94297b8364c26d9a61f
Lidia Diaz Navarro <lyd044@gmail.com>
2018-08-10 08:42:37 +0200

    Imported Audio Activated Audio in intro Completed Credits

---
+++ Assets/MainCameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(106, 68, 15);
+
+
+        }
+	}
+
